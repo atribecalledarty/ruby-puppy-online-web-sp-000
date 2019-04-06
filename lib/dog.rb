5122a6f72cb4581e964c5c8299796e6b7b920511
Arthur Song <arth3rs0ng@gmail.com>
2019-04-06 23:24:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,14 @@ class Dog
     @name = name
     @@all << self
   end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.all 
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
 end
